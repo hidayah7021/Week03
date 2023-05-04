77c4d18f4fa6cc2abb3511ff7d848134ad13a4e5
hidayah7021 <22007021@myrp.edu.sg>
2023-05-04 10:14:50 +0800

    did it liao to section b

--- app/src/main/java/sg/edu/rp/c346/id22007021/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22007021/week03/MainActivity.java
@@ -3,12 +3,24 @@ package sg.edu.rp.c346.id22007021.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Step 1 ; Declaring Variables
+    TextView tvDisplay;
+    Button buttonDisplay;
+    EditText etInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Linking java files to xml files
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        buttonDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
     }
 }
